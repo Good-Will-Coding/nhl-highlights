77cb2a9b5bb4bec6ec1c23fc374e681a350db38b
billy <billyliadphillips@gmail.com>
2019-03-25 21:00:59 +0000

    update

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var jsonServer = require('json-server');
+
+var server = express();
+
+
+server.use('/api', jsonServer.router('db.json'));
+
+server.listen(process.env.PORT || 3000);
